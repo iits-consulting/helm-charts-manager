fce409e8b725f80fb7130e39baf3b0f74b8bef45
Can Aykin <can.aykin@iits-consulting.de>
2020-10-26 13:29:33 +0100

    adjusted tests to use a single configuration and removed dry-run for apply

--- system_test.go
+++ system_test.go
@@ -15,7 +15,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestLint(t *testing.T) {
-	cmd := exec.Command("./helm-charts-manager", "lint", "--update", "--config-file", "./test/test-config-apply.yaml", "--charts-path", "./test/")
+	cmd := exec.Command("./helm-charts-manager", "lint", "--update", "--config-file", "./test/test-config.yaml", "--charts-path", "./test/")
 	result := readByteArray(cmd.CombinedOutput())
 
 	expectedOutput := strings.Split(readByteArray(ioutil.ReadFile("./test/lint-expected-output.txt")), "\n")
@@ -25,7 +25,7 @@ func TestLint(t *testing.T) {
 }
 
 func TestPlan(t *testing.T) {
-	cmd := exec.Command("./helm-charts-manager", "plan", "--update", "--config-file", "./test/test-config-plan.yaml", "--charts-path", "./test/")
+	cmd := exec.Command("./helm-charts-manager", "plan", "--update", "--config-file", "./test/test-config.yaml", "--charts-path", "./test/")
 	result := readByteArray(cmd.CombinedOutput())
 
 	expectedOutput := strings.Split(readByteArray(ioutil.ReadFile("./test/plan-expected-output.txt")), "\n")
@@ -35,7 +35,7 @@ func TestPlan(t *testing.T) {
 }
 
 func TestApply(t *testing.T) {
-	cmd := exec.Command("./helm-charts-manager", "apply", "--auto-approve", "--update", "--config-file", "./test/test-config-apply.yaml", "--charts-path", "./test/")
+	cmd := exec.Command("./helm-charts-manager", "apply", "--auto-approve", "--update", "--config-file", "./test/test-config.yaml", "--charts-path", "./test/")
 	result := readByteArray(cmd.CombinedOutput())
 
 	expectedOutput := strings.Split(readByteArray(ioutil.ReadFile("./test/apply-expected-output.txt")), "\n")
